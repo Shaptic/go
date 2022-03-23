d37806d3dc2c795266dc535168a97c9c6da31e5b
George Kudrayvtsev <george@stellar.org>
2022-03-23 13:24:28 -0700

    Separate test cases, add 0-len test

--- xdr/decorated_signature_test.go
+++ xdr/decorated_signature_test.go
@@ -8,42 +8,43 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestDecoratedSignatures(t *testing.T) {
-	signature := []byte{0, 1, 2, 3, 4, 5, 6, 7, 8}
-	keyHint := [4]byte{9, 10, 11, 12}
+var (
+	signature = []byte{0, 1, 2, 3, 4, 5, 6, 7, 8}
+	hint      = [4]byte{9, 10, 11, 12}
+)
 
+func TestDecoratedSignature(t *testing.T) {
+	decoSig := xdr.NewDecoratedSignature(signature, hint)
+	assert.EqualValues(t, hint, decoSig.Hint)
+	assert.EqualValues(t, signature, decoSig.Signature)
+}
+
+func TestDecoratedSignatureForPayload(t *testing.T) {
 	testCases := []struct {
-		payload             []byte
-		expectedHint        [4]byte
-		expectedPayloadHint [4]byte
+		payload      []byte
+		expectedHint [4]byte
 	}{
 		{
 			[]byte{13, 14, 15, 16, 17, 18, 19, 20, 21},
-			keyHint,
 			[4]byte{27, 25, 31, 25},
 		},
 		{
 			[]byte{18, 19, 20},
-			keyHint,
 			[4]byte{27, 25, 31, 12},
+		},
+		{
+			[]byte{},
+			hint,
 		},
 	}
 
 	for _, testCase := range testCases {
 		t.Run(
-			fmt.Sprintf("%d-byte decorated sig", len(testCase.payload)),
-			func(t *testing.T) {
-				decoSig := xdr.NewDecoratedSignature(signature, keyHint)
-				assert.EqualValues(t, testCase.expectedHint, decoSig.Hint)
-				assert.EqualValues(t, signature, decoSig.Signature)
-			})
-
-		t.Run(
-			fmt.Sprintf("%d-byte decorated sig for payload", len(testCase.payload)),
+			fmt.Sprintf("%d-byte payload", len(testCase.payload)),
 			func(t *testing.T) {
 				decoSig := xdr.NewDecoratedSignatureForPayload(
-					signature, keyHint, testCase.payload)
-				assert.EqualValues(t, testCase.expectedPayloadHint, decoSig.Hint)
+					signature, hint, testCase.payload)
+				assert.EqualValues(t, testCase.expectedHint, decoSig.Hint)
 				assert.EqualValues(t, signature, decoSig.Signature)
 			})
 	}
