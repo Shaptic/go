e3c78da33cceb7f548a909556d0203ea335628ba
George Kudrayvtsev <george@stellar.org>
2022-05-31 16:14:55 -0700

    Cleanup: workers can be specified via the S3 connection URL

--- exp/lighthorizon/index/connect.go
+++ exp/lighthorizon/index/connect.go
@@ -2,8 +2,10 @@ package index
 
 import (
 	"errors"
+	"fmt"
 	"net/url"
 	"path/filepath"
+	"strconv"
 
 	"github.com/aws/aws-sdk-go/aws"
 )
@@ -15,12 +17,27 @@ func Connect(backendUrl string) (Store, error) {
 	}
 	switch parsed.Scheme {
 	case "s3":
+		var parallel uint32 = 20
 		config := &aws.Config{}
 		query := parsed.Query()
 		if region := query.Get("region"); region != "" {
 			config.Region = aws.String(region)
 		}
-		return NewS3Store(config, parsed.Path, 20)
+
+		// Somewhat of a hack: allow control of parallelization via a special
+		// query parameter in the s3:// path.
+		if workers := query.Get("workers"); workers != "" {
+			workerCount, err := strconv.ParseUint(workers, 10, 32)
+			if err != nil {
+				return nil, fmt.Errorf("failed to parse worker count (%s): %v", workers, err)
+			}
+			if workerCount > 0 {
+				parallel = uint32(workerCount)
+			}
+			query.Del("workers")
+		}
+
+		return NewS3Store(config, parsed.Path, parallel)
 
 	case "file":
 		return NewFileStore(filepath.Join(parsed.Host, parsed.Path), 20)
