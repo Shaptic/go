12ca5de7c40493c3bf676389c459525ef5bc815a
George Kudrayvtsev <george@stellar.org>
2022-06-02 13:24:03 -0700

    Flush account list before dropping the in-memory store:
    If this isn't done, then FlushAccounts() will do absolutely nothing after a
    Flush(), because the previous Flush() will clear the map of indices out of
    memory.

--- exp/lighthorizon/index/store.go
+++ exp/lighthorizon/index/store.go
@@ -60,12 +60,7 @@ func (s *store) accounts() []string {
 func (s *store) FlushAccounts() error {
 	s.mutex.Lock()
 	defer s.mutex.Unlock()
-
-	if err := s.backend.FlushAccounts(s.accounts()); err != nil {
-		return err
-	}
-
-	return nil
+	return s.backend.FlushAccounts(s.accounts())
 }
 
 func (s *store) Read(account string) (map[string]*CheckpointIndex, error) {
@@ -97,6 +92,10 @@ func (s *store) Flush() error {
 	defer s.mutex.Unlock()
 
 	if err := s.backend.Flush(s.indexes); err != nil {
+		return err
+	}
+
+	if err := s.backend.FlushAccounts(s.accounts()); err != nil {
 		return err
 	}
 
@@ -159,6 +158,7 @@ func (s *store) AddParticipantsToIndexesNoBackend(checkpoint uint32, index strin
 			return err
 		}
 	}
+
 	return nil
 }
 
