91c62f58b2357bcb2b70f3cc69c00d93aef76723
George Kudrayvtsev <george@stellar.org>
2022-07-20 14:56:43 -0700

    "Fix" the data race by not doing any flushes until the end

--- exp/lighthorizon/index/builder.go
+++ exp/lighthorizon/index/builder.go
@@ -127,13 +127,8 @@ func BuildIndices(
 				}
 
 				nprocessed := atomic.AddUint64(&processed, uint64(count))
-				if nprocessed%19 == 0 {
+				if nprocessed%97 == 0 {
 					printProgress("Reading ledgers", nprocessed, uint64(ledgerCount), startTime)
-				}
-
-				// Upload indices once per checkpoint to save memory
-				if err := indexStore.Flush(); err != nil {
-					return errors.Wrap(err, "flushing indices failed")
 				}
 			}
 			return nil
